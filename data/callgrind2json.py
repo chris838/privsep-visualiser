f2a21ddf33641f935bbe40b5b953931eefc2e24a
Chris Harding <chris.harding@acm.org>
2011-09-05 15:34:02 +0100

    Fixed bug causing output to hang

--- data/callgrind2json.py
+++ data/callgrind2json.py
@@ -5,43 +5,48 @@ import os
 import time
 import re
 import json
+import sys
 
-data_path = "updates.json"
-outfile="callgrind.out"
-logfile="valgrind.out"
+# Log/output files
+vg_outfile="valgrind.out"
+pg_outfile="privgrind.out"
 wastefile="waste.gz"
 
-# Hold the run of costs so far
-costs = {}
+# Remove output from previous run
+os.system( "rm {} {} {}".format(vg_outfile, pg_outfile, wastefile) )
 
+"""
 # Function to process dump file
 def process(dump) :
 	print "Processing new callgrind dump file"
-	os.system( "callgrind_annotate {}".format(outfile) )
+"""
 
 
-# Remove output from previous run
-os.system( "rm {} {} {}".format(outfile, logfile, wastefile) )
 
+"""
 # Create output pipe
-os.system( "mkfifo {}".format(outfile) )
+os.system( "mkfifo {}".format(cg_outfile) )
+"""
 
 # Create valgrind/callgrind call
-cg_call  = "valgrind --tool=callgrind "
-cg_call += "--log-file={} ".format(logfile)
-cg_call += "--callgrind-out-file={} ".format(outfile)
-cg_call += "--compress-pos=no "
-cg_call += "--dump-every-bb=50000000 "
-cg_call += "--combine-dumps=yes "
-cg_call += "gzip-clanged -c 5MB.zip > {} ".format(wastefile)
+cg_call  = "( "
+cg_call += "valgrind --tool=privgrind "
+cg_call += "--log-file={} ".format(vg_outfile)
+cg_call += "gzip-clanged test"
+cg_call += " )"
 
+os.system( cg_call )
+
+"""
 # Fork child process to run callgrind
 pid = os.fork()
 if pid :
-	# Parent process - reads pipe into callgrind_annotate until child is finished
-	while True :
-		os.system( 'echo "cmd:  gzip-clanged -c 5MB.zip"|cat - {} > /tmp/out; callgrind_annotate /tmp/out'.format(outfile) )
+	sys.exit()
+	# Parent process - reads pipe until child is finished
+	#while True :
+		#os.system( 'echo "cmd:  gzip-clanged -c 5MB.zip"|cat - {} > /tmp/out; callgrind_annotate /tmp/out'.format(outfile) )
 else :
 	# Child process - calls valgrind to write data to the pipe
 	os.system( cg_call )
 	sys.exit(0)
+"""
--- data/test_gzip.sh
+++ data/test_gzip.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
-# Output here
-outfile="dumps/callgrind.out"
-
-# Wipe directory
-rm dumps/*
-
-# Create output pipe
-mkfifo $outfile
+# 
 
 # Run valgrind/callgrind
-valgrind --tool=callgrind \
-	--log-file=valgrind.out \
-	--callgrind-out-file=$outfile \
-	--compress-pos=no \
-	--dump-every-bb=500000 \
-	--combine-dumps=no \
-	\
-	gzip-clanged -c 5MB.zip > dumps/waste.gz
+privgrind --log-file=data.json gzip-clanged *.bin && gunzip *.gz
